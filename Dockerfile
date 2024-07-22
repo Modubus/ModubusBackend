64e69bc9afb5ef065b2847b6d4be0a0e5ff6be8a
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-07-22 09:20:17 +0900

    feat: add start.sh

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:20-alpine
+
+RUN apk add --no-cache bash
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install
+
+CMD ["bash"]
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+### "chmod +x start.sh" - 실행 권한 부여 후 이 파일을 실행해주세요.
+
+docker-compose up -d
+docker exec -it modubus bash
