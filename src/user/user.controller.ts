8d5f48c785e252aea6a30a005706031fe9afe431
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-07-24 20:46:59 +0900

    feat: use prisma and create user, bus, prisma module (#1)
    * feat: project setting
    - eslint 세팅
    - setup script 세팅
    
    * feat: use prisma
    
    * docs: add readme
    
    * feat: add prisma, user, bus module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Prisma } from '@prisma/client'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UserService } from './user.service'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  /**
+   * 모든 유저 정보를 불러옵니다.
+   */
+  @Get()
+  async getUsers() {
+    try {
+      return await this.userService.getUsers()
+    } catch (error) {
+      console.log(error)
+      throw new HttpException('Unknown Error', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  /**
+   * 새로운 유저 정보를 저장합니다.(회원가입)
+   * @param createUserDto 유저 정보
+   */
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.userService.createUser(createUserDto)
+    } catch (error) {
+      console.log(error)
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new HttpException(
+          'User already exists or invalid request',
+          HttpStatus.CONFLICT,
+        )
+      }
+      throw new HttpException('Unknown Error', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client'
+import { CreateUserDto } from './dto/create-user.dto'
+import { Injectable } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async getUsers() {
+    return await this.prisma.user.findMany()
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10)
+
+    const user = await this.prisma.user.create({
+      data: {
+        username: createUserDto.username,
+        password: hashedPassword,
+        email: createUserDto.email,
+        disableType: createUserDto.disableType,
+      },
+    })
+
+    return user
+  }
+}
