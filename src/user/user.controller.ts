2a11b2bbe4f368ad75b3697b3b5f5de3b3aab960
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-08-01 17:32:53 +0900

    feat: implement user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,78 @@
 import { Prisma } from '@prisma/client'
-import { CreateUserDto } from './dto/create-user.dto'
+import { CreateUserDto, LoginUserDto, UpdateUserDto } from './dto/user.dto'
 import { UserService } from './user.service'
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpException,
   HttpStatus,
   Post,
+  Put,
+  Req,
+  Res,
+  UseGuards,
 } from '@nestjs/common'
+import { RequestWithUser } from 'src/lib/class/request-with-user.class'
+import { Request, Response } from 'express'
+import { JwtAuthGuard } from 'src/lib/guard/jwt-auth.guard'
 
 @Controller('user')
 export class UserController {
+  REFRESH_TOKEN_NAME = 'refresh_token'
   constructor(private readonly userService: UserService) {}
 
+  @Post('login')
+  async login(
+    @Body() loginUserDto: LoginUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    // id, pw 확인
+    if (!(await this.userService.validateUser(loginUserDto))) {
+      throw new HttpException(
+        'check your username or password again',
+        HttpStatus.UNAUTHORIZED,
+      )
+    }
+
+    // jwt 발급
+    const tokens = await this.userService.issueJwtTokens(loginUserDto)
+    res.setHeader('authorization', `Bearer ${tokens.accessToken}`)
+    res.cookie(this.REFRESH_TOKEN_NAME, tokens.refreshToken)
+  }
+
   /**
-   * 모든 유저 정보를 불러옵니다.
+   * Access Token이 만료된 경우, Refresh Token을 사용해 Access Token을 재발급합니다.
+   * @returns
+   */
+  @Get('reissue')
+  async reissueAccessToken(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken = req.cookies[this.REFRESH_TOKEN_NAME]
+    if (!refreshToken) {
+      throw new HttpException(
+        'There is no refresh token',
+        HttpStatus.UNAUTHORIZED,
+      )
+    }
+
+    // jwt 재발급
+    const tokens = await this.userService.reissueAccessToken(refreshToken)
+    res.setHeader('authorization', `Bearer ${tokens.accessToken}`)
+    res.cookie(this.REFRESH_TOKEN_NAME, tokens.refreshToken)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie(this.REFRESH_TOKEN_NAME)
+  }
+
+  /**
+   * 모든 유저 정보를 불러옵니다. (테스트용 기능, 삭제 예정)
    */
   @Get()
   async getUsers() {
@@ -42,6 +99,47 @@ export class UserController {
           'User already exists or invalid request',
           HttpStatus.CONFLICT,
         )
+      }
+      throw new HttpException('Unknown Error', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  /**
+   * 유저의 정보를 수정합니다.
+   * @param updateUserDto 수정할 유저 정보
+   */
+  @UseGuards(JwtAuthGuard)
+  @Put()
+  async updateUser(
+    @Req() req: RequestWithUser,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    try {
+      return await this.userService.updateUser(req.user.userId, updateUserDto)
+    } catch (error) {
+      console.log(error)
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new HttpException(
+          'invalid userId or invalid update request',
+          HttpStatus.BAD_REQUEST,
+        )
+      }
+      throw new HttpException('Unknown Error', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  /**
+   * 유저를 제거합니다.
+   */
+  @UseGuards(JwtAuthGuard)
+  @Delete()
+  async deleteUser(@Req() req: RequestWithUser) {
+    try {
+      return await this.userService.deleteUser(req.user.userId)
+    } catch (error) {
+      console.log(error)
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new HttpException('User does not exist', HttpStatus.NOT_FOUND)
       }
       throw new HttpException('Unknown Error', HttpStatus.INTERNAL_SERVER_ERROR)
     }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
+import { JwtModule } from '@nestjs/jwt'
+import { JwtStrategy } from 'src/lib/strategy/jwt.strategy'
+import { JwtAuthGuard } from 'src/lib/guard/jwt-auth.guard'
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtStrategy, JwtAuthGuard],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '10m' }, // default value
+    }),
+  ],
 })
 export class UserModule {}
