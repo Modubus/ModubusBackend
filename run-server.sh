b463865ca2910e587f39a44578c1dae4af79af7f
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-08-01 23:05:18 +0900

    feat: add background-run-server script (#8)
    * feat: add background-run-server script
    
    * docs: modify readme

--- run-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### "chmod +x start-server.sh" - 실행 권한 부여 후 이 파일을 실행해주세요.
-### 서버 실행
-
-cp .env.development .env
-npx prisma generate
-npm run start
---
+++ scripts/run-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### "chmod +x run-server.sh" - 실행 권한 부여 후 이 파일을 실행해주세요.
+### 서버 실행
+
+./scripts/setup.sh
+npm run start
