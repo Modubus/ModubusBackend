b8825c05ecde970684ec64fda6c1b8c187cb6691
dania0x4C <ljm31881004@gmail.com>
2024-08-03 07:34:57 +0900

    delete sensitive Info

--- src/api/node-api/node-api.service.ts
+++ src/api/node-api/node-api.service.ts
@@ -54,9 +54,3 @@ export class NodeApiService {
     return null
   }
 }
-
-//https://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteNoList?serviceKey=GwEsh%2FRZ5k9%2F%2FvaCKgymKhM3TqaHvZ0EQONjhs%2FX1advH8ozuEtGyni4l7FbWcld87nRrxPb%2BW%2Fu4%2FXwZbc7tA%3D%3D&pageNo=1&numOfRows=10&_type=json&cityCode=25&routeNo=100
-//여기에서 본인 노선 번호 다시 확인 후 nodeId 받아오기
-
-//https://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteAcctoThrghSttnList?serviceKey=GwEsh%2FRZ5k9%2F%2FvaCKgymKhM3TqaHvZ0EQONjhs%2FX1advH8ozuEtGyni4l7FbWcld87nRrxPb%2BW%2Fu4%2FXwZbc7tA%3D%3D&pageNo=1&numOfRows=10&_type=xml&cityCode=25&routeId=DJB30300004
-//그리고 여기에서 노선 아이디로경로 추출 후 json반환
