602b283acec1a3b1c885fca68f61a29c2a190b8a
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-08-05 13:54:31 +0900

    feat: add user-favorite, user-needs apis (#11)
    * feat: use cookie-parser
    
    * fix: fix typo in prisma schema
    
    * feat: add user-favorite apis
    
    * feat: add user-needs apis

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import * as dotenv from 'dotenv'
-
-dotenv.config()
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  dotenv.config()
+  app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
 }
