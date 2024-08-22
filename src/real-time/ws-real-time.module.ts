b19c99d13801f83d81588cbb4e87b4ec4cf381e1
dania0x4C <ljm31881004@gmail.com>
2024-08-22 20:44:00 +0900

    Ignore src/real-time directory

--- src/real-time/ws-real-time.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { EventsGateway } from './ws-real-time.gateway'
-import { EventsService } from './ws-real-time.service'
-
-@Module({
-  providers: [EventsGateway, EventsService],
-})
-export class EventsModule {}
